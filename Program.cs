dd287d7bf5ec888ec90bf5e546d05a763c78dcda
NikitkaGL <75216078+NikitkaGL@users.noreply.github.com>
2020-11-29 21:49:38 +0200

    Lab(1)
    Lab(1)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            double t;
+            Console.WriteLine("ctg^-1*(x-pi)");
+            Console.WriteLine("Введите значение для х");
+            x=Console.ReadLine();
+            t = Convert.ToDouble(x);
+            double y = Math.Pow(Math.Tan(t - Math.PI), -1) ;
+            Console.WriteLine("ctg^-1("+x+"-pi)="+y);
+        }
+    }
+}
+
